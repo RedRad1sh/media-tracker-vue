e42c6b457f043d16f7b3f689680645a44ff76019
Nikita_Kucherenko <ext_nkucherenko@rencredit.ru>
2023-12-22 22:37:44 +0300

    edit routes

--- src/router/router.js
+++ src/router/router.js
@@ -17,27 +17,27 @@ const routes = [
         component: MainPage
     },
     {
-        path: '/ProfilePage',
+        path: '/profile',
         meta: { title: 'Профиль', showHeader: true },
         component: ProfilePage
     },
     {
-        path: '/FilmsPage',
+        path: '/movies',
         meta: { title: 'Фильмы', showHeader: true },
         component: FilmsPage
     },
     {
-        path: '/GamesPage',
+        path: '/games',
         meta: { title: 'Игры', showHeader: true },
         component: GamesPage
     },
     {
-        path: '/BooksPage',
+        path: '/books',
         meta: { title: 'Книги', showHeader: true },
         component: BooksPage
     },
     {
-        path: '/RecommendationsPage',
+        path: '/recomendations',
         meta: { title: 'Рекомендации', showHeader: true },
         component: RecommendationsPage
     },
@@ -52,7 +52,7 @@ const routes = [
         component: CurrentGame
     },
     {
-        path: '/UserLists',
+        path: '/profile/lists',
         meta: { title: 'Личные списки', showHeader: true },
         component: UserLists
     },
