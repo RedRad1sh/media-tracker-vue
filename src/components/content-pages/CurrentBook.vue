3187e12f6692e09f1104927f837e6918d6f9f949
Sivolap <sivolapik2018@gmail.com>
2024-01-04 22:03:47 +0300

    fix - Add component for current book

--- src/components/content-pages/CurrentBook.vue
+++ src/components/content-pages/CurrentBook.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="block">
-    <MenuComponent id="menu-include" active-element="0" />
+    <MenuComponent id="menu-include" active-element="1"/>
     <div class="content-main">
       <div class="block-row" style="justify-content: center">
-        <div class="content-header">{{bookObj.title}}</div>
+        <div class="content-header">{{ bookObj.title }}</div>
       </div>
       <div class="obj-first-row">
         <div class="obj-img-element">
@@ -16,7 +16,7 @@
         <div class="obj-info-element">
           <div class="info-header">Информация</div>
           <div class="obj-info">
-            <InfoforCurrentPage :titles="bookInfo" :values="bookInfoObj" />
+            <InfoforCurrentPage :titles="bookInfo" :values="bookInfoObj"/>
           </div>
         </div>
       </div>
@@ -24,7 +24,7 @@
         <div class="obj-rating-element">
           <div class="info-header">Рейтинг</div>
           <div class="obj-info">
-            <InfoforCurrentPage :typeinfo="'RATING'" :titles="bookRating" :values="bookRatingObj" />
+            <InfoforCurrentPage :typeinfo="'RATING'" :titles="bookRating" :values="bookRatingObj"/>
           </div>
         </div>
         <div class="obj-rating-element">
@@ -33,13 +33,13 @@
             <span class="text-format">
               Оценка:
             </span>
-            <SelectionMark id="rate" v-model="selectedValMark"> </SelectionMark>
+            <SelectionMark id="rate" v-model="selectedValMark"></SelectionMark>
           </div>
           <div class="obj-actions">
             <span class="text-format">
               Списки:
             </span>
-            <SelectionContent :ObjectType="type"> </SelectionContent>
+            <SelectionContent :ObjectType="type"></SelectionContent>
           </div>
         </div>
       </div>
@@ -47,21 +47,20 @@
         <div class="obj-description-element">
           <div class="info-header">Описание</div>
           <div class="text-block">
-            {{bookObj.description}}
+            {{ bookObj.description }}
           </div>
         </div>
       </div>
       <div class="create-review">
         <div class="create-review-container">
           <label class="text-block text-block-otz" for="review-field"
-          >Оставьте отзыв к книге:</label
-          >
+          >Оставьте отзыв к книге:</label>
           <br/>
           <textarea
               id="review-field"
               class="frame3-frame-inputlabels"
-              v-model = "textareamsg"
-          ></textarea>
+              v-model="textareamsg">
+          </textarea>
         </div>
         <button
             id="review-button"
@@ -73,7 +72,7 @@
       </div>
 
       <div class="review-block">
-        <ReviewMessage :revobj="rev" v-for = "rev in reviews" :key="rev.id"/>
+        <ReviewMessage :revobj="rev" v-for="rev in reviews" :key="rev.id"/>
       </div>
     </div>
   </div>
@@ -90,7 +89,7 @@ import {config} from '@/config/config.js';
 
 export default {
   name: "CurrentBook",
-  components: { MenuComponent, SelectionMark, SelectionContent, InfoforCurrentPage, ReviewMessage},
+  components: {MenuComponent, SelectionMark, SelectionContent, InfoforCurrentPage, ReviewMessage},
 
   data() {
     return {
@@ -99,17 +98,23 @@ export default {
       bookInfo: ['Тип:', 'Категория:', 'Автор:', 'Издательство:', 'Год издания:', 'Количество страниц:'],
       bookRating: ['Оценка MediaTracker:'],
 
-      reviews: [{id: 1, username: 'whatIsLove',   profurl: "https://avtozaryad.ru/local/templates/main/assets/images/user.png", mark: 10, text: 'Baby don\'t hurt me'},],
+      reviews: [{
+        id: 1,
+        username: 'whatIsLove',
+        profurl: "https://avtozaryad.ru/local/templates/main/assets/images/user.png",
+        mark: 10,
+        text: 'Baby don\'t hurt me'
+      },],
       selectedValMark: '-',
       textareamsg: '',
     };
   },
-  
+
   mounted() {
     this.getBookbyId();
   },
 
-  computed:{
+  computed: {
     bookInfoObj() {
       return ['Книга',
         this.bookObj.category,
@@ -125,23 +130,27 @@ export default {
   },
 
   methods: {
-    getBookbyId(){
+    getBookbyId() {
       let backendUrl = `${config.backend.url}/books/book/` + this.$route.params.id;
-      console.log(backendUrl);
 
       axios.get(backendUrl)
           .then(response => {
             this.bookObj = response.data;
-            console.log(this.bookObj);
           })
           .catch(error => {
             console.error('Ошибка получения данных с бекенда', error);
           });
     },
 
-    createMessageElement(){
-      if (this.selectedValMark !== '-' && this.textareamsg !== ''){
-        this.reviews.push({id: null, username: 'newuser', profurl: "https://avtozaryad.ru/local/templates/main/assets/images/user.png", mark: this.selectedValMark, text: this.textareamsg});
+    createMessageElement() {
+      if (this.selectedValMark !== '-' && this.textareamsg !== '') {
+        this.reviews.push({
+          id: null,
+          username: 'newuser',
+          profurl: "https://avtozaryad.ru/local/templates/main/assets/images/user.png",
+          mark: this.selectedValMark,
+          text: this.textareamsg
+        });
       }
     },
 
@@ -151,6 +160,6 @@ export default {
 </script>
 
 <style scoped>
-  @import "~@/assets/css/cur-obj-page.scss";
+@import "~@/assets/css/cur-obj-page.scss";
 </style>
   
